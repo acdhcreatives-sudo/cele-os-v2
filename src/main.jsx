e6275c7af924cb7530159552cce286adac0648ce
Monica De Castro <acdh.creatives@gmail.com>
2026-06-30 13:32:09 +0800

    Enable PWA install

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { registerSW } from "virtual:pwa-register";
+
 import "./index.css";
 import App from "./App";
+
+// Register Service Worker
+registerSW({
+  immediate: true,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
